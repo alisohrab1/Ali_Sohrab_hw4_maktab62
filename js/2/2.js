a39c66f20f385b60c0e26bbcc3be32c0963b4419
alisohrab1 <alisohrab230@gmail.com>
2021-10-19 01:36:46 +0330

    changes made to file 2 of js folder

--- js/2/2.js
+++ js/2/2.js
@@ -5,18 +5,18 @@ let str = "dad";
 
 function isSame1(str)
 {
-    let reverse = str.toLowerCase().split(" ").reverse().join("");
+    let reverse = str.toLowerCase().split("").reverse().join("");
     return result = (str === reverse) ? true : false;
 }
 
-console.log(isSame1('dad'));
+console.log(isSame1(str));
 
 /////////////////////////////////////////////////////////////second solution////////////////////////////////////////////////////////
 console.log('second soloution: for multiple words');
 let myStr = "race car"
 function isSame2(str){
 
-    let reverse = str.toLowerCase().split(" ").join("").split(" ").reverse().join("");
+    let reverse = str.toLowerCase().split(" ").join("").split("").reverse().join("");
     return result = (str.toLowerCase().split(" ").join("") === reverse) ? true : false;
 }
 console.log(isSame2(myStr));
